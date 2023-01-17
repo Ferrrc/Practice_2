a8d810c6d84904cdf9cf334977de7fae8b77afee
Fer <ruizcorofer.06qgmail.com>
2023-01-17 18:36:21 -0500

    Condicion negativo, positivo o cero

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int numerolf = 0;
+
+        if (numerolf > 0) {
+            System.out.println("Este numero es positivo");
+        } else if (numerolf < 0) {
+            System.out.println("Este numero es negativo");
+        } else {
+            System.out.println("Este numero es cero");
+        }
+
+
+    }
+}
